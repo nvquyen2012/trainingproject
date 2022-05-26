117436151d2c393184b0848da776959f813df6c4
giang1599-nguyen <17130047@st.hcmuaf.edu.vn>
2022-05-26 11:30:31 +0700

    fix pom mapstruct and update code get API status investor

--- training-base/src/main/java/com/example/trainingbase/mapper/InvestorStatusMapper.java
+++ training-base/src/main/java/com/example/trainingbase/mapper/InvestorStatusMapper.java
@@ -1,21 +1,28 @@
 package com.example.trainingbase.mapper;
 
+import com.example.trainingbase.dto.InvestorStatusDto;
 import com.example.trainingbase.entity.crm.InvestorIndividual;
+import com.example.trainingbase.entity.crm.InvestorInstitutional;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring",
-        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
-        uses = {InvestorStatusMapper.class})
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface InvestorStatusMapper {
-//    @Mapping(target = "erpId", source = "organizationErpId")
-//    @Mapping(target = "name", source = "organizationName")
-//    @Mapping(target = "status", source = "status", qualifiedByName = "status.toEntity")
-//    InvestorIndividual toEntity(OrganizationDto organization);
-//
-//    @Mapping(target = "organizationErpId", source = "erpId")
-//    @Mapping(target = "organizationName", source = "name")
-//    @Mapping(target = "status", source = "status", qualifiedByName = "status.toDto")
-//    OrganizationDto toDto(Organization organizationCreated);
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "status", source = "status")
+    InvestorIndividual toIndividualEntity(InvestorStatusDto investorStatusDto);
+
+    @Mapping(target = "name", source = "name")
+    @Mapping(target = "status", source = "status")
+    InvestorStatusDto toIndividualDto(InvestorIndividual investorIndividual);
+
+    @Mapping(target = "companyName", source = "name")
+    @Mapping(target = "status", source = "status")
+    InvestorInstitutional toInstitutionalEntity(InvestorStatusDto investorStatusDto);
+
+    @Mapping(target = "name", source = "companyName")
+    @Mapping(target = "status", source = "status")
+    InvestorStatusDto toInstitutionalDto(InvestorInstitutional investorIndividual);
 }
