d914d8b7051b5f5a8b7fa6eed4b586d8a379e769
giang1599-nguyen <17130047@st.hcmuaf.edu.vn>
2022-05-26 11:40:09 +0700

    delete unnecessary code

--- training-base/src/main/java/com/example/trainingbase/mapper/InvestorStatusMapper.java
+++ training-base/src/main/java/com/example/trainingbase/mapper/InvestorStatusMapper.java
@@ -10,19 +10,13 @@ import org.mapstruct.NullValuePropertyMappingStrategy;
 @Mapper(componentModel = "spring",
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface InvestorStatusMapper {
-    @Mapping(target = "name", source = "name")
-    @Mapping(target = "status", source = "status")
     InvestorIndividual toIndividualEntity(InvestorStatusDto investorStatusDto);
 
-    @Mapping(target = "name", source = "name")
-    @Mapping(target = "status", source = "status")
     InvestorStatusDto toIndividualDto(InvestorIndividual investorIndividual);
 
     @Mapping(target = "companyName", source = "name")
-    @Mapping(target = "status", source = "status")
     InvestorInstitutional toInstitutionalEntity(InvestorStatusDto investorStatusDto);
 
     @Mapping(target = "name", source = "companyName")
-    @Mapping(target = "status", source = "status")
     InvestorStatusDto toInstitutionalDto(InvestorInstitutional investorIndividual);
 }
