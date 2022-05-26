11d906cdecd6d5f25724107548f353a9ee4f7a98
giang1599-nguyen <17130047@st.hcmuaf.edu.vn>
2022-05-26 14:57:27 +0700

    D8 - Update get properties of entity and Dto to get investor status

--- training-base/src/main/java/com/example/trainingbase/dto/InvestorStatusDto.java
+++ training-base/src/main/java/com/example/trainingbase/dto/InvestorStatusDto.java
@@ -2,8 +2,12 @@ package com.example.trainingbase.dto;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Data
 public class InvestorStatusDto {
+    private String investorId;
     private String name;
     private String status;
+    private LocalDateTime createdAt;
 }
